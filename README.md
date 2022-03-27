be7a7a4e66f76d2e7fc0d9482840728273e3f42f
Bohdan Lisnenko <lem8r.ka11a@gmail.com>
2022-03-27 13:54:39 +0300

    saas template: common for all odoo versions

--- README.md
+++ README.md
@@ -1,29 +1,11 @@
-# README #
+# saas-template
 
-This README would normally document whatever steps are necessary to get your application up and running.
+Environment variables on repository level.
 
-### What is this repository for? ###
+- `O_MAJOR` - Odoo major version: 15, 14, ...
+- `O_EDITION` - ee or ce edition. Default ee.
+- `SAAS_RELEASE` - Odoo release version of SaaS image. Default to `O_RELEASE`.
+- `WORKERS_COUNT` - number of scaled docker services. Default 1.
+- `DOMAIN` - set custom domain. It has to be configured with CNAME reference before enabling here.
 
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
-
-### How do I get set up? ###
-
-* Summary of set up
-* Configuration
-* Dependencies
-* Database configuration
-* How to run tests
-* Deployment instructions
-
-### Contribution guidelines ###
-
-* Writing tests
-* Code review
-* Other guidelines
-
-### Who do I talk to? ###
-
-* Repo owner or admin
-* Other community or team contact
+Create staging branch to deploy staging service.
---
+++ tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+git submodule init
+git submodule update --remote
+export REPO="erpukraine/custom" PROJECT="${BITBUCKET_REPO_SLUG}" VERSION="v1.0"
+if [[ -v BITBUCKET_BUILD_NUMBER ]]; then VERSION=${BITBUCKET_BUILD_NUMBER}; fi
+export IMAGE_NAME=${REPO}:${PROJECT}-${VERSION}
+export UPDATED_MODULES=`ls ./extra-addons/ | awk -vORS=, '{ print $1 }' | sed 's/,$/\n/'`
+if [ -z ${UPDATED_MODULES} ]; then echo "Nothing to test"; exit 0; fi
+echo "Testing ${UPDATED_MODULES}"
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+sed -i -e 's/without_demo = True/without_demo = False/g' odoo.conf
+sed -i -e 's/use_redis = .*/use_redis = False/g' odoo.conf
+sed -i -e "s/db_host =.*/db_host = host.docker.internal/g" odoo.conf
+docker build --build-arg SAAS_IMG=erpukraine/custom:odoo-15.0ee-saas-erpu-latest -t ${IMAGE_NAME} .
+mkdir ${BITBUCKET_CLONE_DIR}/data && chmod 777 ${BITBUCKET_CLONE_DIR}/data
+docker run --rm -t --name=${PROJECT}-${VERSION} \
+    --add-host host.docker.internal:${BITBUCKET_DOCKER_HOST_INTERNAL} \
+    -v ${BITBUCKET_CLONE_DIR}/data:/var/lib/odoo \
+    -e "HOST=host.docker.internal" ${IMAGE_NAME} \
+    -i base,web,${UPDATED_MODULES} -d test-db \
+    --db_host=host.docker.internal --db-filter=test-db \
+    -w odoo -r odoo --workers=0 --stop-after-init
+docker run --rm -t --name=${PROJECT}-${VERSION} \
+    --add-host host.docker.internal:${BITBUCKET_DOCKER_HOST_INTERNAL} \
+    -v ${BITBUCKET_CLONE_DIR}/data:/var/lib/odoo -e "HOST=host.docker.internal" \
+    -e "COVERAGE_FILE=/tmp/.coverage" -e "MARICHKA_KEY=${MARICHKA_KEY}" ${IMAGE_NAME} \
+    /bin/bash -c "set -e; coverage run /usr/bin/odoo \
+    -u ${UPDATED_MODULES} --workers=0 -d test-db --stop-after-init --test-enable -w odoo -r odoo \
+    --db_host host.docker.internal --db-filter=test-db; coverage report \
+    --omit *system_site_packages*,*site-packages*,*dist-packages*,*pyshared*,*enterprise-addons* "
